8d85e9bc76753251953a31f130595a34472e1de2
RAYENkc <kacerayen13@gmail.com>
2021-01-20 13:09:08 +0100

    some init msg

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_1/page/qr_create_page.dart';
+import 'package:flutter_application_1/page/qr_scan_page.dart';
+import 'package:flutter_application_1/widget/button_widget.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'QR Code Scanner';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        home: MainPage(title: title),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  final String title;
+
+  const MainPage({
+    @required this.title,
+  });
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  String _value = "";
+  Future scanQR() async {
+    try {
+      print('heyyy rayen 1111111111111');
+      String cameraScanResult = await scanner.scan();
+      print(cameraScanResult); // The result type (barcode, cancelled, failed)
+
+      print('heyyy rayen 2222222222222222222');
+
+      setState(() {
+        _value = cameraScanResult;
+      });
+    } catch (e) {}
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ButtonWidget(
+                  text: 'Create QR Code',
+                  onClicked: () => Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => QRCreatePage(),
+                  )),
+                ),
+                const SizedBox(height: 32),
+                ButtonWidget(
+                  text: 'Scan QR Code',
+                  onClicked: () => scanQR(),
+                ),
+                const SizedBox(height: 32),
+                Text(
+                  _value,
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+}
